66e166760881eb0dd99d896f2954e54650b70266
evidencemabunda <132655866+evidencemabunda@users.noreply.github.com>
2023-12-05 10:45:15 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {StyleSheet, Text,View } from "react-native";
+
+export default function App(){
+  return(
+    <View style={styles.container}>
+      <Text>Welcome to Zaka app</Text>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginHorizontal: 16,
+  },
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+});
+
---
+++ login.jsx
@@ -0,0 +1,8 @@
+export const Login = () => {
+    return (
+        <form>
+            <label for="email">Email</label>
+            <input type="email" placeholder="email@gmail.com" id="email" name="email" />
+        </form>
+    )
+}
